752104690167d70d4b7ae0a755d1fc4feff645c2
Zed <regreting117@naver.com>
2023-06-27 00:12:57 +0900

    헤더를 꾸몃서요~

--- practice1/src/App.vue
+++ practice1/src/App.vue
@@ -27,5 +27,13 @@ export default defineComponent({ // 아래있는 내용들을 내보낸다 (객�
 </template>
 
 <style scoped>
-
+@font-face {
+    font-family: 'LeeSeoyun';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2202-2@1.0/LeeSeoyun.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+.main{
+  font-family: 'LeeSeoyun';
+}
 </style>
--- practice1/src/components/Header.vue
+++ practice1/src/components/Header.vue
@@ -1,7 +1,25 @@
 <template>
-    <div style="width: 100%; height: 100%; background-color: #6f76f3; max-height: 350px; margin: 0px; padding: 10px; color:#FFFFFF; font-size: 10px;">
-      ((알바때문에))피곤한데 재밋다
+  <div>
+    <div
+      style="margin-top: 15px; margin-bottom: 15px; align-items: center; width: 100%; height: 100%; background-color: #ffffff; max-height: 15px; color:#000000; font-size: 10px; display: flex;">
+      <div style="font-size: 20px; font-weight: 650; cursor: pointer; margin-left: 20px;">
+        사이버기록장
+      </div>
+      <div style="font-size: 15px; font-weight: 0; cursor: pointer; margin-left: auto; margin-right: 20px;">
+        날개
+      </div>
+      <div style="font-size: 15px; font-weight: 0; cursor: pointer; margin-right: 20px;">
+        날개
+      </div>
+      <div style="font-size: 15px; font-weight: 0; cursor: pointer; margin-right: 20px;">
+        날개
+      </div>
+      <div style="font-size: 15px; font-weight: 0; cursor: pointer; margin-right: 20vw;">
+        날개
+      </div>
     </div>
+  </div>
+  <hr />
 </template>
 
 <script>
